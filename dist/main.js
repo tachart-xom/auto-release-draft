be7809199cd83e429b37f400339ddaaf04122f95
Tim Chartrand <timothy.a.chartrand@exxonmobil.com>
2023-12-26 16:37:18 -0600

    after build

--- dist/main.js
+++ dist/main.js
@@ -25,22 +25,14 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.run = void 0;
 const core = __importStar(require("@actions/core"));
-const wait_1 = require("./wait");
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
     try {
-        const ms = core.getInput('milliseconds');
-        // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-        core.debug(`Waiting ${ms} milliseconds ...`);
-        // Log the current timestamp, wait, then log the new timestamp
-        core.debug(new Date().toTimeString());
-        await (0, wait_1.wait)(parseInt(ms, 10));
-        core.debug(new Date().toTimeString());
-        // Set outputs for other workflow steps to use
-        core.setOutput('time', new Date().toTimeString());
+        core.notice('hello world! from main.ts');
+        core.setOutput('release-url', 'https://example.com');
     }
     catch (error) {
         // Fail the workflow run if an error occurs
